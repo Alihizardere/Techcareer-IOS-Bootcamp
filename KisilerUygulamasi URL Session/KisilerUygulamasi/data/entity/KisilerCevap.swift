7e0abb0d359a726d9ecb9b1ec5d6e2c55c532677
AliHizardere <aliduyguhzr@gmail.com>
2023-11-12 13:26:30 +0300

    contacts app url session

---
+++ KisilerUygulamasi URL Session/KisilerUygulamasi/data/entity/KisilerCevap.swift
@@ -0,0 +1,13 @@
+//
+//  KisilerCevap.swift
+//  KisilerUygulamasi
+//
+//  Created by alihizardere on 1.10.2023.
+//
+
+import Foundation
+
+class KisilerCevap : Codable {
+    var kisiler:[Kisiler]?
+    var success:Int?
+}
---
+++ KisilerUygulamasi URL Session/KisilerUygulamasi/ui/views/KisiDetay.swift
@@ -0,0 +1,34 @@
+//
+//  KisiDetay.swift
+//  KisilerUygulamasi
+//
+//  Created by alihizardere on 21.09.2023.
+//
+
+import UIKit
+
+class KisiDetay: UIViewController {
+    @IBOutlet weak var tfKisiAd: UITextField!
+    @IBOutlet weak var tfKisiTel: UITextField!
+    
+    var kisi:Kisiler?
+    
+    var viewModel = KisiDetayViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let k = kisi {
+            tfKisiAd.text = k.kisi_ad
+            tfKisiTel.text = k.kisi_tel
+        }
+        
+    }
+    
+    @IBAction func buttonGuncelle(_ sender: Any) {
+        if let ad = tfKisiAd.text , let tel = tfKisiTel.text , let k = kisi{
+            viewModel.guncelle(kisi_id: Int(k.kisi_id!)!, kisi_ad: ad, kisi_tel: tel)
+        }
+    }
+    
+    
+}
